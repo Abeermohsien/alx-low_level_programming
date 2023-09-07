c27bf85cc845fbae7511e1df15723fd218b5af52
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-07 12:37:47 -0700

    last task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -7,11 +6,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	 printf("size of char %1d bytes\n", sizeof(char));
-        printf("size of int %d bytes\n", sizeof(int));
-        printf("size of long %11d bytes\n", sizeof(long int));
-        printf("size of short int %11d bytes\n", sizeof(short int));
-        printf("size of float %1d bytes\n", sizeof(float));
+{pintf("size of char %1d bytes\n", sizeof(char));
+printf("size of int %d bytes\n", sizeof(int));
+printf("size of long %11d bytes\n", sizeof(long int));
+printf("size of short int %11d bytes\n", sizeof(short int));
+printf("size of float %1d bytes\n", sizeof(float));
     return (0);
 }
