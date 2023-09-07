4889bf336928ac25a5b19ddc17ed04b060b7de7a
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-07 10:47:50 -0700

    first c programmes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+int main(void)
+{
+	print("size of char %d bytes\n", sizeof(char));
+	print("size of int %d bytes\n", sizeof(int));
+	print("size of long %d bytes\n", sizeof(long int);
+	print("size of short int %d bytes\n", sizeof(short int);
+	print("size of float %d bytes\n", sizeof(float);
+	return(0);
+	}	
