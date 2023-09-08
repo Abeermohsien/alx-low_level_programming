36a0e81f44337cd9948fc820bb390d0428f0dd46
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-08 09:04:41 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha = 'a', ALPHA = 'A';
+while (alpha <= 'z' && ALPHA <= 'Z')
+{
+putchar(alpha);
+putchar(ALPHA)
+alpha ++
+ALPHA ++
+}
+putchar('\n');
+return (0);
+}
