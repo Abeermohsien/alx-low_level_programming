29b5be831a6f4a8535fba6ec132d0405156c37ad
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-08 08:49:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+char alpha = 'a' ;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+ /* your code goes there */
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n')
+return (0);
+}
+
