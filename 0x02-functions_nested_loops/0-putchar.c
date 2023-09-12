b236aba9e3518fc0b902aeaa8c7e69b727b13545
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 08:00:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,17 @@
  *
  * Return: Always 0 (Success)
  */
+int _putchar(char c)
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+_putchar(char '_');
+_putchar(char 'p');
+_putchar(char 'u');
+_putchar(char't');
+_putchar(char 'c');
+_putchar(char 'h');
+_putchar(char 'a');
+_putchar(char 'r');
+_putchar(char '\n');
 return (0);
 }
