e564c4cc0af1b66fa42f97adc15c7f24cdbcef34
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 14:11:36 -0700

    kk

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,22 @@
  */
 void jack_bauer(void)
 {
-int hour, minutes;
-for (hour = 0; hour <= 23; hour++)
+int a, b, c, d;
+for (a = 0; a <= 2; a++)
 {
-for (minutes = 0; minutes <= 60; minutes++)
+for (b = 0; b <= 60; b++)
 {
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+{
+for (c = 0; c <= 5; c++)
+{
+for (d = 0; d <= 9; d++)
+{	
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((minutes / 10) + '0');
 _putchar((minutes % 10) + '0');
 _putchar('\n');
-}
 }
 }
