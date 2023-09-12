97873f88746d617c5213268bfdb5f52e7efe9b9f
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 10:43:51 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar('+');
+}
+if (n == 0)
+{
+return (0);
+_putchar(0);
+}
+if (n < 0)
+{
+return (-1);
+_putchar(-);
+}
+}
