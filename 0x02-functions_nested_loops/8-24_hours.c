5f430667c9a40e026a76efd1a215307c29d35b16
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 14:17:21 -0700

    kk

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - counts the mintes in jack_bauer day
- * 
+ *
  *
  * Return: Always 0.
  */
@@ -18,7 +18,7 @@ if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
-{	
+{
 _putchar(a + '0');
 _putchar(b + '0');
 _putchar(58);
