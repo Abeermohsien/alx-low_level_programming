04974b9b6c586a0fd282821d21bb09417d1dece2
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 05:57:26 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,6 @@
+#include "main.h"
+int main()
+{
+_putchar("_putchar");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+void _putchar(char str[])
+{
+int c;
+char str[c];
+c = 0;
+while (str[c] != '\n')
+{
+putchar(str);
+str[c++];
+}
+return;
+}
