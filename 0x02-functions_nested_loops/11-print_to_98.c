33ac963578f8583a71eb4b40951c2d1003c8eba4
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 12:25:33 -0700

    mmm

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+int i, start;
+printf("enter anumber:");
+scanf("%d", &start);
+for (i = start; i <= 98; i++)
+{
+prinf("%d, ", i);
+}
+return (0);
+}
