d691c7e4f79174524a0b9cc70b9228cd3bffd193
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 08:37:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+for(char c = 'a'; c <= 'z'; c++)
+{
+_putchar('c');
+}
+_purchar('\n');
+return (0);
+}
