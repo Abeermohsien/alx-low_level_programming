48528a485505a3734b3baeb9ef348e958a6dcf84
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 09:13:10 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0.
  */
-void  print_alphabet(void)
+void print_alphabet(void);
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
@@ -13,5 +13,4 @@ for (c = 'a'; c <= 'z'; c++)
 _putchar('c');
 }
 _putchar('\n');
-return (0);
 }
