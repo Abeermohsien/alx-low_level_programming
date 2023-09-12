bcbdda8fe942f54fb41f54a6874d665589ec60dc
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-12 09:29:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet: prints alphabets in low-case from a to z
- * 
+ * print_alphabet - writes alphabets in low-case from
+ *
  * Return: Always 0.
  */
 void print_alphabet(void)
