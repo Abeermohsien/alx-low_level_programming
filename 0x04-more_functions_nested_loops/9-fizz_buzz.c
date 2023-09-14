bd03906081dd1a5c38cf5bfbee1231b7ad127f79
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-14 11:17:18 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,7 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz");
-}
-else if (i % 3 == 0 && !(i % 5 == 0))
+if (i % 3 == 0 && !(i % 5 == 0))
 {
 printf("Fizz");
 }
@@ -23,12 +19,16 @@ else if (i % 5 == 0 && !(i % 3 == 0))
 {
 printf("Buzz");
 }
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
 else
 {
 printf("%d", i);
 }
 printf(" ");
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
