75e8f17bed9ab16b905cd3d69598025aec55501c
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-18 09:31:31 -0700

    kkk

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,18 +8,10 @@
 void puts2(char *str)
 {
 int i;
-i = 0;
-while (str[i] != '\0')
+for (i = 0; str[i] != '\0'; ++i)
 {
 if (i % 2 == 0)
-{
-_putchar(str[i]);
-++i;
-}
-else
-{
-break;
-}
+	_putchar(str[i]);
 }
 _putchar('\n');
 }
