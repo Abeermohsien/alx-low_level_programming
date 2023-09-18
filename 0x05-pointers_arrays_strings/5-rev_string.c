0257e468dfc5d22ffdf7aceb9ff2e12fc67bda54
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-18 08:51:56 -0700

    kkk

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * rev_string - reverse of string
+ *
+ * @s: pointer character
+ */
+void rev_string(char *s)
+{
+int i, len, temp;
+len = strlen(s);
+for (i = 0; i < len/2; i++)
+{
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+}
+}
