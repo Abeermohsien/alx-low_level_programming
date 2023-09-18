3e51da11b61adaf3b98589013d30b35eac06fc31
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-18 09:14:30 -0700

    ll

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts - prints every other chacter
+ *
+ * @str: string of character
+ */
+void puts2(char *str)
+{
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
+}
