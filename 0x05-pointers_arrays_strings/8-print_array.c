b06a2256d6e43b05693c00332ce5541a257759b8
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-18 10:10:39 -0700

    ll

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,12 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
-_putchar(a[i]);
-printf(", ");
+if (i != (n-1))
+	printf("%d, ", a[i]);
+else
+	printf("%d", a[i]);
 }
 printf("\n");
 }
