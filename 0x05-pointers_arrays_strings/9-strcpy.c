91ebf911c235c57fcb13d4d4072b167d4424e251
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-18 10:25:05 -0700

    lll

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that copies the string pointed 
+ * @src: string chacters
+ * @dest: string characters
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = -1; src[i] != '\0'; i++)
+{
+dest[i] = scr[i];
+}
+return (scr);
+}
