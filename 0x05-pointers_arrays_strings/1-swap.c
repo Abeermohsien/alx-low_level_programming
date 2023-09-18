34b1bd4a00aa6122a18baaf3d6b84ab57c650f10
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-18 04:29:25 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * swap_int - swap the vlues of two integars
+ * @a: integr pointer
+ * @b: integar pointer
+ * **/
+void swap_int(int *a, int *b)
+{
+int d = *a;
+a = *b;
+b = d;
+}
