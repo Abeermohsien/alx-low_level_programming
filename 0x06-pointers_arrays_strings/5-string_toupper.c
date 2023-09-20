b0409b3acbe6883c602afdc6144e1373fdcf11df
Abeermohsien <aboorakhalil993@gmail.com>
2023-09-20 12:18:03 -0700

    kk

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,7 +5,13 @@
  * 
  * Return: Always 0.
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-int i, n;
-:ً
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 97 && str[i] <= 122)
+	str[i] = str[i] - 32;
+}
+return (str);
+}
