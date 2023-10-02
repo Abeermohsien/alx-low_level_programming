bdab1c8b3b1ce3ba5b38050190e435255cc07249
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-02 06:02:19 -0700

    lll

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,18 +4,18 @@
  *
  * @s: string
  */
-int _atio(char *s)
+int _atoi(char *s)
 {
 unsigned int num = 0;
 int sign = 1;
 do
 {
 if (*s == '-')
-sign *= -1;
+	sign *= -1;
 else if (*s >= '0' && *s <= '9')
-num = (num * 10) + (*s - '0');
+	num = (num * 10) + (*s - '0');
 else if (num > 0)
-break;
+	break;
 }
 while (*s++);
 return (num * sign);
