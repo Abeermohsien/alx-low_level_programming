86a7e3a6085fe339146b161af09cbde489e5a9b6
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-02 08:05:13 -0700

    lllll

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ * main - print the result of addition
+ * @argc: int
+ * @argv: char
+ * Return: 0.
+ *
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+char *p;
+while (--argc)
+{
+for (p = argv[argc]; *p; p++)
+	if (*p < '0' && *p > '9')
+	return (printf("Error\n"), 1);
+sum += atoi(argv[argc]);
+}
+printf("%d\n", sum);
+return (0);
+}
