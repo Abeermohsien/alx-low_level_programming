f7192a070a397617ffe45c8d7f29f741a4c2c7fe
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-02 07:33:11 -0700

    llllll

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all the arguments with new line
+ * @argc: int
+ * @argv: char
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+while (arg--)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}
