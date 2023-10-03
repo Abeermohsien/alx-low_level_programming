72d62ef1e01c264d5717fa83696f8f0de0fd615b
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-03 07:34:41 -0700

    kkkk

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - print array of certin size
+ * @c: char
+ * @size: the size of the memory to print
+ *
+ * Return: char.
+ */
+char *create_array(unsigned int size, char c)
+{
+char *ptr = (char*) malloc(size * sizeof(char));
+if (size == 0 || ptr == 0)
+	return (0);
+while (size != 0)
+{
+*ptr = c;
+size--;
+ptr++;
+}
+return (ptr);
+}
