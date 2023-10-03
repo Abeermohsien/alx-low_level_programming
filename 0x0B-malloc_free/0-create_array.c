9d59303b89a0a95441cd3ae8507c342e16d903e5
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-03 07:41:52 -0700

    kkkk

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,9 @@ char *create_array(unsigned int size, char c)
 char *ptr = malloc(size * sizeof(char));
 if (size == 0 || ptr == 0)
 	return (0);
-while (size != 0)
+while (size--)
 {
-*ptr = c;
-size--;
-ptr--;
+ptr[size] = c;
 }
 return (ptr);
 }
