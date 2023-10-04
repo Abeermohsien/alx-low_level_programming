0426920d4798c0fe1b2d258c69b0eaeb1bd7071b
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-04 10:19:20 -0700

    kkk

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * counter - count letter numbers
+ * @s: string
+ * Return: int
+ */
+int counter(char *s)
+{
+int i, n = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+	if (s[i] == ' ')
+	{
+		if (s[i + 1] != '\0')
+		n++;
+	}
+	else if (i == 0)
+		n++;
+}
+n++;
+return (n);
+}
+/**
+ * strtow - sapeterate two strings
+ * @str: string
+ * Return: char
+ */
+char **strtow(char *str)
+{
+	char **w;
+	int k, l, i = 0, n = 0, m = 0;
+if (str == NULL || *str != '\0')
+	return (NULL);
+n = counter(str);
+w = (char **)malloc(n *sizeof(char *));
+while (str[i])
+{
+	if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+	{
+		for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+			;
+		j++;
+		w[m] = (char *)malloc(j * sizeof(char));
+		j--;
+		if (w[m] == NULL)
+		{
+			for (c = 0; c < m; c++)
+				free(w[c]);
+			free(w[n - 1]);
+			free(w);
+			return (NULL);
+		}
+		for (l = 0; l < j; l++)
+			w[m][l] = str[i + 1];
+		w[m][l] = '\0';
+		m++;
+		i += j;
+	}
+	else
+		i++;
+}
+return (w);
+}
