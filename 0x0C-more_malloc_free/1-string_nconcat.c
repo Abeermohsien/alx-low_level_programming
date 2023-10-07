e1942e91c0b58c6210a9deb4ab4dd20a89b05522
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-07 08:11:50 -0700

    lllll

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,18 +13,18 @@ char *ptr;
 unsigned int i, m, j, k;
 if (s1 == NULL)
 {
-s1 = " ";
+s1 = "";
 }
 if (s2 == NULL)
 {
-s2 = " ";
+s2 = "";
 }
 for (i = 0; s1[i] != '\0'; i++)
 ;
 for (m = 0; s2[m] != '\0'; m++)
 ;
 ptr = malloc(i + n + 1);
-if (ptr == 0)
+if (ptr == NULL)
 return (NULL);
 for (j = 0; s1[j] != '\0'; j++)
 ptr[j] = s1[j];
