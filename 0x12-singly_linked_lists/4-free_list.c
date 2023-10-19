3a427c3e0dc15e7667aa6a6f94f185c91add47dd
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-19 08:11:49 -0700

    ll

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - free th list
+ * @head: pointer
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+list_t *nd, *next;
+if (!head)
+	return;
+nd = head;
+while (nd)
+{
+next = next->next;
+free(nd->str);
+free(nd);
+node = next;
+}
+}
