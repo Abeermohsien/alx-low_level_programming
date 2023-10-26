f56b75dcf13dd3fe79c4d244186d668bec37d992
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-26 11:05:37 -0700

    $

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit -clears bit
+ * @n: int
+ * @index: int
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	if (*n & 1l << index)
+		*n ^= 1l << index;
+	return (1);
+}
