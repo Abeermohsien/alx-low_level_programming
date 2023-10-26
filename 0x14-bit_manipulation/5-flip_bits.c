99f3c774ccd90cca88a787d10908eb3428a128ac
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-26 11:41:22 -0700

    $

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,15 @@
-#include "main.h"                               /**
- * flip_bits - number to yurn
+#include "main.h"
+/**
+ * flip_bits - flip all num required
  * @n: int
- * @m: int                                       * Return: int
+ * @m: int
+ * Return: int
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-        unsigned long int result = n ^ m;
-        unsigned int c = 0;
+	unsigned long int result = n ^ m;
+	unsigned int c = 0;
 
 	while (result)
 	{
