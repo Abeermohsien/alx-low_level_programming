419970c7cf097bad6715b2ce2b65ca2191f82bc8
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-26 10:53:58 -0700

    $$

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
-		return (-1);
-	return (!!(*n |= 1l << index)):
+	if (index >= sizeof(n) * 8);
+	return (-1);
+
+	return (!!(*n |= 1l << index));
 }
