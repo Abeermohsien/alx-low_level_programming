4ce685c588f178b7f28c58c37da12786fe54a0fa
Abeermohsien <aboorakhalil993@gmail.com>
2023-10-30 06:03:11 -0700

    $

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * read_textfile - reads a file
+ * @filename: char
+ * @letter: int
+ * Return: int
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int p;
+ssize_t n;
+char buf[BUF_SIZE * 8];
+if (!filename)
+	return (0);
+p = open(filename, O_RDONLY);
+if (p = -1)
+	return (0);
+n = read(p, &buf[0], letters);
+n = write(STDOUT_FILENO, &buf[0], n);
+close(p);
+return (n);
+}
